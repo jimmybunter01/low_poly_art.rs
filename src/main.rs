5156e40fe3135c0cf7c49d2d96bad489f90463b6
jimmybunter01 <65281539+jimmybunter01@users.noreply.github.com>
2023-07-03 23:47:24 +0100

    Started working on the rejection sampling.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rand::{Rng, distributions::Uniform};
 // use delaunator::{Point, Triangulation};
-use image::{GrayImage, ImageBuffer, DynamicImage};
+use image::{GrayImage, ImageBuffer};
+use rand_distr::Distribution;
 
 fn main() {
     const PICTURE_FILE_PATH: &str = "C:\\Users\\James\\Documents\\Projects\\low_poly_art\\galaxy.jpg";
@@ -9,7 +10,7 @@ fn main() {
     greyscale_image.save("images\\test.jpg").unwrap();
     let x = greyscale_image.blur(2.0);
     let x2 = greyscale_image.blur(30.0);    
-    highlight_details(x.to_luma8(), x2.to_luma8())
+    highlight_details(x.to_luma8(), x2.to_luma8());
     sample_verticies_from_image
 }
 
@@ -43,18 +44,32 @@ fn highlight_details(image_one: GrayImage, image_two: GrayImage) {
     }
 
     final_image_buffer.save("images\\diff_image.jpg").unwrap();
-
 }
 
-fn sample_verticies_from_image(image: DynamicImage) {
-    let mut n = 1000000;
-    let mut rng = rand::thread_rng();
+fn sample_verticies_from_image(image: GrayImage) {
+    const NUM_OF_NUMBERS_TO_GEN: usize = 1000000;
+    const NUM_OF_FLOATS_TO_GEN: f32 = 1000000.0;
     let (width, height) = image.dimensions();
+    let mut xs : [u32; NUM_OF_NUMBERS_TO_GEN];
+    let mut ys : [u32; NUM_OF_NUMBERS_TO_GEN];
+    
+    let mut rng = rand::thread_rng();
+    let x_dist = Uniform::from(0..width);
+    let y_dist = Uniform::from(0..height);
+   
+    for i in 0..NUM_OF_NUMBERS_TO_GEN {
+        xs[i] = x_dist.sample(&mut rng);
+        ys[i] = y_dist.sample(&mut rng);
+    }
 
-
-
-
-    let xs = rng.gen_range(0..width);
-    let ys = rng.gen_range(0..height);
+    for x in 0..xs {
+        for y in 0..ys {
+            let random_value_dist = Uniform::from(0.0..NUM_OF_FLOATS_TO_GEN);
+            let random_value = random_value_dist.sample(&mut rng);
+            let pixel_value = image.get_pixel(x, y)[0];
+            let accept = random_value < pixel_value;
+            
+        }
+    } 
 }
  
